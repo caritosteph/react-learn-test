73e3c78fdd7920f3b5010c352408090ca5bdeacd
Carolyn <carito.ulfe@gmail.com>
2017-03-05 01:21:21 -0500

    adding actions and reomve

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes} from 'react';
 
 class CoursesPage extends Component {
+
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      course: { title: null }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(e){
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({course});
+  }
+
+  onClickSave(){
+    console.log("Save");
+  }
+
   render() {
     return (
       <div>
-        Courses
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type = ""
+          onChange = {this.onTitleChange}
+          value = {this.state.course.title} />
+        <input
+          type = "submit"
+          value = "Save"
+          onClick = {this.onClickSave} />
       </div>
     );
   }
